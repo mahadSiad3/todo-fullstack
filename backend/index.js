821c8e5c8a1c7a9a3256f19fcf4be094eec2c9d8
mahadsiad3 <mahadsiad3@gmail.com>
2025-07-11 10:05:08 -0400

    fetched data

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,15 @@
 import express from 'express'
+import cors from 'cors'
 
 const app = express()
 
 const port = 3000
 
+app.use(cors())
+
 app.get('/', (req,res)=>{
-    res.send('Hello {from Server}')
+    //res.send('Hello {from Server}')
+    res.json('Hello {from Server}')
 })
 
 app.listen(port, ()=> console.log('listening on port: ' + port))
