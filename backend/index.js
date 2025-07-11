aa74201d895982552988e7386c31f02e4906bcea
mahadsiad3 <mahadsiad3@gmail.com>
2025-07-11 12:09:07 -0400

    adding list items for todo list

--- backend/index.js
+++ backend/index.js
@@ -5,16 +5,39 @@ import 'dotenv/config'
 
 import connectDb from './db.js'
 
+import Todo from './modules/todo.js'
+
 const app = express()
 
 const port = process.env.PORT
 
 app.use(cors())
+app.use(express.json())
 
-app.get('/', (req,res)=>{
+app.get('/todos', async(req,res)=>{
     //res.send('Hello {from Server}')
-    res.json('Hello {from Server}')
-    connectDb()
+
+    try {
+        const todos = await Todo.find({})
+
+   // res.json('Hello {from Server}')
+        res.status(200).json(todos)
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+
+app.post('/todos', async(req,res)=>{
+    // const todo = new Todo(req.body) //creates it to memory on the server
+    // await todo.save() //actually saves it
+
+    try {
+        const todo = await Todo.create(req.body)
+        res.status(200).json(todo)
+    } catch (error) {
+        console.log(error)
+        res.status(400).json(error)
+    }
 })
 
 app.listen(port, ()=> {
---
+++ backend/modules/todo.js
@@ -0,0 +1,23 @@
+import { text } from "express";
+import mongoose, { mongo } from "mongoose";
+
+const todoSchema = mongoose.Schema({
+    text :{type: String},
+    completed: {type: Boolean, default : false}
+})
+
+const Todo = mongoose.model('todo',todoSchema)
+
+export default Todo
+
+
+
+
+
+
+
+
+
+
+
+
