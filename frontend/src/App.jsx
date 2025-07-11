cd8481631cc3b62fb4248b0681acf84eb3a27d98
mahadsiad3 <mahadsiad3@gmail.com>
2025-07-11 12:09:48 -0400

    adding list items from origin terminal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,21 @@
 import { useEffect } from 'react'
 import './App.css'
+import { useState } from 'react'
+import { useRef } from 'react'
 
 function App() {
+
+  const [todos,setTodos]= useState([])
+const inputref=  useRef()
 
   useEffect(() => {
 
     async function getData() {
       try {
-        const response = await fetch('http://localhost:3000')
+        const response = await fetch('http://localhost:3000/todos')
         const data = await response.json()
         console.log(data) 
+        setTodos(data)
       } catch (e) {
         console.log(e)
       }
@@ -19,9 +25,46 @@ function App() {
 
   }, [])
  
+  console.log(todos)
+
+  async function handleSumbit(event) {
+    event.preventDefault()
+
+    const todo = {
+      text: inputref.current.value
+    }
+    //to send to the server use a post request 
+
+    const response = await fetch('http://localhost:3000/todos',{
+      method:'POST',
+      body: JSON.stringify(todo),
+      headers:{
+        'content-type':'application/json'
+      }
+    })
+
+    const newTodo = await response.json()
+
+    console.log(newTodo)
+
+    setTodos([...todos, newTodo])
+
+  }
+
   return (
     <>
-      Hello (from frontend)
+    <h1>Todos</h1>
+      {/* Hello (from frontend) */}
+      <form onSubmit={handleSumbit}>
+      <input type="text"  ref={inputref}/>
+      <button>Submit</button>
+      </form>
+
+      <ul>
+        {todos.map(todo =>
+          <li key={todo._id}>{todo.text}</li>
+        )}
+      </ul>
     </>
   )
 }
